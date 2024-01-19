b8ae9863674caf4e162d4ceac2c24d058851751e
YeBeKhe-Helper <129661618+yebekhehelper@users.noreply.github.com>
2024-01-20 00:17:17 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,16 @@ self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open('v1').then(cache => {
 			cache.addAll([
+                                '/',
 				'/index.html',
 				'/style.css',
 				'/script.js',
+                                'favicon.svg',
+				'/icon-32x32.png',
+				'/icon-64x64.png',
+				'/icon-128x128.png',
 				'/icon-192x192.png',
+				'/icon-256x256.png',
 				'/icon-512x512.png'
 			]);
 		})
